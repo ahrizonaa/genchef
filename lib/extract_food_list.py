5315c8231c659f415137103381d2986735af5331
missahrizona <princessssahri@gmail.com>
2022-12-18 21:31:50 -0600

    extracted all dish images

---
+++ lib/extract_food_list.py
@@ -0,0 +1,66 @@
+from bs4 import BeautifulSoup
+import re
+import json
+import requests
+
+lines = None
+with open('genshin_food_list.html', 'r') as file:
+    lines = file.readlines()
+
+
+html_text = '\n'.join(lines)
+
+soup = BeautifulSoup(html_text, 'html.parser')
+
+rows = soup.find_all('tr')
+
+dishes = []
+
+for row in rows:
+    cells = row.find_all('td')
+    img = cells[0].find('img')
+    # children = [node for node in row.contents if node != '\n']
+    if img is not None or img != -1:
+        dish = {}
+        src = img.get('src')
+        data_src = img.get('data-src')
+        if re.search(r'https', src) is not None:
+            dish['src'] = re.sub(r'\s+', ' ', src.replace('\n', '')).strip()
+        else:
+            dish['src'] = re.sub(
+                r'\s+', ' ', data_src.replace('\n', '')).strip()
+
+    title = cells[1].find('a')
+    if title is not None:
+        dish['name'] = re.sub(
+            r'\s+', ' ', title.text.replace('\n', '')).strip().replace(r'\\', '').replace('"', '')
+
+    stars = cells[2].find('img')
+    if stars is not None:
+        dish['stars'] = re.sub(
+            r'\s+', ' ', stars.get('alt').replace('\n', '')).strip()
+
+    effect_title = cells[3].find('a')
+    if effect_title is not None:
+        dish['effect_title'] = re.sub(
+            r'\s+', ' ', effect_title.get('title').replace('\n', '')).strip()
+
+    type = cells[3].text
+    dish['type'] = re.sub(r'\s+', ' ', type.replace('\n', '')).strip()
+
+    description = cells[4].text
+    dish['description'] = re.sub(
+        r'\s+', ' ', description.replace('\n', '')).strip()
+
+    dishes.append(dish)
+    if dish['src'] is not None or dish['src'] != '':
+        try:
+            img_data = requests.get(dish['src']).content
+            with open('./dishes/' + dish['name'] + '.png', 'wb') as handler:
+                handler.write(img_data)
+        except:
+            print('skipping')
+
+
+with open('genshin_food_list.json', 'w') as file:
+    file.write(json.dumps(dishes, indent=4))
